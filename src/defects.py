e9066839e0a600ab4c2bc9fd5444f93078e3e59f
Malcolm Ramsay <malramsay64@gmail.com>
2018-07-27 09:15:57 +1000

    ENH Create defects helper module
    This is a module of helper functions for the analysis and creation of
    crystal defects. These functions have been taken from
    notebooks/06_Defect_Creation.ipynb and
    notebooks/07_Defect_thermodynamics.ipynb and will be later imported back
    into the notebooks to remove much of the code making them more readable.
    Additionally in bringing the functions into a python module they are
    easier to test and there is less reuse of code, with both notebooks now
    able to use these functions.

---
+++ src/defects.py
@@ -0,0 +1,107 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2018 Malcolm Ramsay <malramsay64@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""Helper functions for the creation and analysis of defects."""
+
+from hoomd.data import SnapshotParticleData, make_snapshot
+from sdanalysis.figures import configuration
+from sdanalysis.frame import HoomdFrame
+from sdrun.params import SimulationParams
+
+
+def remove_molecule(snapshot: SnapshotParticleData, index: int) -> SnapshotParticleData:
+    """Remove an arbitratry molecule from the simulation.
+
+    This also ensures a valid configuration once the molecule has been removed.
+    """
+    mask = snapshot.particles.body != index
+    if sum(mask) == snapshot.particles.N:
+        print("Index not in snapshot")
+        return snapshot
+
+    new_snapshot = make_snapshot(
+        snapshot.particles.N - 3,
+        snapshot.box,
+        snapshot.particles.types,
+        snapshot.pairs.types,
+    )
+    snapshot_attributes = [
+        "position",
+        "angmom",
+        "velocity",
+        "orientation",
+        "acceleration",
+        "image",
+        "mass",
+        "moment_inertia",
+        "typeid",
+    ]
+
+    for attr in snapshot_attributes:
+        getattr(new_snapshot.particles, attr)[:] = getattr(snapshot.particles, attr)[
+            mask
+        ]
+    body_mask = snapshot.particles.body != max(snapshot.particles.body)
+    new_snapshot.particles.body[:] = snapshot.particles.body[body_mask]
+    return new_snapshot
+
+
+def central_molecule(run_params: SimulationParams) -> int:
+    """Find the molecule closest to the center of the simulation.
+
+    This finds the unit cell halfway along each of the axes, then multiplies
+    by 2 since there are 2 molecules in each unit cell.
+
+    """
+    x, y, z = run_params.cell_dimensions
+    return int(x / 2 * y + y / 2) * 2
+
+
+def remove_vertical(
+    snapshot: SnapshotParticleData, run_params: SimulationParams, num_mols: int
+) -> SnapshotParticleData:
+    center = central_molecule(run_params)
+    for index in range(center - 2 * int(num_mols / 2), center):
+        snapshot = remove_molecule(snapshot, index)
+    return snapshot
+
+
+def remove_horizontal(
+    snapshot: SnapshotParticleData, run_params: SimulationParams, num_mols: int
+) -> SnapshotParticleData:
+    center = central_molecule()
+    x, y, x = run_params.cell_dimensions
+    extent = 2 * int(num_mols / 2) * y
+
+    for index in range(center - extent, center + extent, (y - 1) * 2):
+        snapshot = remove_molecule(snapshot, index)
+        snapshot = remove_molecule(snapshot, index + 2)
+    return snapshot
+
+
+def _plot_snapshot(snapshot: SnapshotParticleData):
+    """Helper function to plot a single snapshot."""
+    return configuration.plot_frame(HoomdFrame(snapshot))
+
+
+def plot_snapshots(snapshots, num_rows=2):
+    # Length of sides to make a square
+    num_columns = int(len(snapshots) / num_rows)
+    figures = []
+    for i in range(num_rows):
+        row = []
+        for j in range(num_columns):
+            if i * num_columns + j > len(snapshots):
+                figures.append(row)
+                return gridplot(figures)
+            fig = _plot_snapshot(snapshots[i * num_columns + j])
+            fig.plot_height = int(fig.plot_height / num_rows)
+            fig.plot_width = int(fig.plot_width / num_rows)
+            row.append(fig)
+        figures.append(row)
+    return gridplot(figures)
