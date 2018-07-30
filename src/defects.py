359c148e171e25a7413516b64e15cf3329c232db
Malcolm Ramsay <malramsay64@gmail.com>
2018-07-30 17:53:34 +1000

    NB Writeup and analysis of the defect behaviour.
    This is an analysis of the more appropriate defect behaviour. The
    previous analysis was on a system that was not initially at thermal
    equilibrium and so there were some unusual effects. This writeup
    includes the thermal effects and is looking at multiple sizes of defect.

--- src/defects.py
+++ src/defects.py
@@ -12,6 +12,7 @@ from bokeh.layouts import gridplot
 from hoomd.data import SnapshotParticleData, make_snapshot
 from sdanalysis import HoomdFrame
 from sdanalysis.figures import configuration
+from sdanalysis.order import knn_model, compute_ml_order
 from sdrun import SimulationParams
 
 
@@ -75,22 +76,28 @@ def remove_vertical(
 def remove_horizontal(
     snapshot: SnapshotParticleData, run_params: SimulationParams, num_mols: int
 ) -> SnapshotParticleData:
-    center = central_molecule()
+    center = central_molecule(run_params)
     x, y, x = run_params.cell_dimensions
     extent = 2 * int(num_mols / 2) * y
 
-    for index in range(center - extent, center + extent, (y - 1) * 2):
+    for index in range(center - extent, center, (y - 1) * 2):
         snapshot = remove_molecule(snapshot, index)
         snapshot = remove_molecule(snapshot, index + 2)
     return snapshot
 
 
-def _plot_snapshot(snapshot: SnapshotParticleData):
+def _plot_snapshot(snapshot: SnapshotParticleData, order: bool = False):
     """Helper function to plot a single snapshot."""
-    return configuration.plot_frame(HoomdFrame(snapshot))
+    frame = HoomdFrame(snapshot)
+    if order:
+        from functools import partial
+
+        order = partial(compute_ml_order, knn_model())
+        return configuration.plot_frame(frame, order)
+    return configuration.plot_frame(frame)
 
 
-def plot_snapshots(snapshots, num_rows=2):
+def plot_snapshots(snapshots, num_rows: int = 2, order: bool = False):
     # Length of sides to make a square
     num_columns = int(len(snapshots) / num_rows)
     figures = []
@@ -100,7 +107,7 @@ def plot_snapshots(snapshots, num_rows=2):
             if i * num_columns + j > len(snapshots):
                 figures.append(row)
                 return gridplot(figures)
-            fig = _plot_snapshot(snapshots[i * num_columns + j])
+            fig = _plot_snapshot(snapshots[i * num_columns + j], order=order)
             fig.plot_height = int(fig.plot_height / num_rows)
             fig.plot_width = int(fig.plot_width / num_rows)
             row.append(fig)
