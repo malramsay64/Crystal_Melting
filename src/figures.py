af207af84b562dca758d4eb9a469708669ab5921
Malcolm Ramsay <malramsay64@gmail.com>
2019-05-11 09:58:44 +1000

    feat: Create figures src holding themes
    Standardising the theme of the figures is rather important, so this
    creates a figures module containing my custom themes for altiar.
    Additionally this will eventually hold all the standard functions for
    generating figures

---
+++ src/figures.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2019 Malcolm Ramsay <malramsay64@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""Module containing function for theming and generating figures."""
+
+from typing import Any, Dict
+
+
+def my_theme() -> Dict[str, Any]:
+    """Define an Altair theme to use in all visualisations.
+
+    This defines a simple theme, specifying the aspect ratio of 4:6
+    and removing the grid from the figure which is distracting.
+
+    """
+    return {"config": {"view": {"height": 400, "width": 600}, "axis": {"grid": False}}}
+
+
+def use_my_theme(alt):
+    """Register and my custom Altair theme."""
+    # register and enable the theme
+    alt.themes.register("my_theme", my_theme())
+    alt.themes.enable("my_theme")
