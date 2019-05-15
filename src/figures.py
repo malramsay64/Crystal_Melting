2a6a6d3d9ef8de2eaa719a0d1142d0470c50b3ed
Malcolm Ramsay <malramsay64@gmail.com>
2019-05-15 15:09:04 +1000

    analysis: Helper functions for adding h/vlines
    Adding a horizontal or vertical line to an altair chart is rather
    difficult and requies a number of lines of code. This creates a couple
    of helper functions to create either horizontal or vertical lines at a
    specified value on the chart.

--- src/figures.py
+++ src/figures.py
@@ -10,6 +10,8 @@
 
 from typing import Any, Dict
 
+import altair.vegalite.v2 as alt
+
 
 def my_theme() -> Dict[str, Any]:
     """Define an Altair theme to use in all visualisations.
@@ -21,8 +23,36 @@ def my_theme() -> Dict[str, Any]:
     return {"config": {"view": {"height": 400, "width": 600}, "axis": {"grid": False}}}
 
 
-def use_my_theme(alt):
+def use_my_theme():
     """Register and my custom Altair theme."""
     # register and enable the theme
-    alt.themes.register("my_theme", my_theme())
+    alt.themes.register("my_theme", my_theme)
     alt.themes.enable("my_theme")
+
+
+def _add_line(chart: alt.Chart, value: float, line: alt.Chart) -> alt.Chart:
+    data = chart.data
+    if data == alt.Undefined:
+        try:
+            for layer in chart.layer:
+                if layer.data is not alt.Undefined:
+                    data = layer.data
+                    break
+            else:
+                raise RuntimeError("No data found in Chart object")
+        except AttributeError:
+            raise RuntimeError("No data found in Chart object")
+
+    return alt.layer(chart, line, data=data).transform_calculate(val=f"{value}")
+
+
+def hline(chart: alt.Chart, value: float) -> alt.Chart:
+    """Draw a horizontal line on an Altair chart object."""
+    line = alt.Chart().mark_rule(color="grey").encode(y="val:Q")
+    return _add_line(chart, value, line)
+
+
+def vline(chart: alt.Chart, value: float) -> alt.Chart:
+    """Draw a vertical line on an Altair chart object."""
+    line = alt.Chart().mark_rule(color="grey").encode(x="val:Q")
+    return _add_line(chart, value, line)
