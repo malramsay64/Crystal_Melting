8dd74e05b4b0fed2afacca1ecb8979abfa07d67f
Malcolm Ramsay <malramsay64@gmail.com>
2019-06-25 08:46:23 +1000

    analysis(melting): Reduce minimum volume in cleanup
    With the change to the analysis that I am taking into account the
    changing surface area, this reduces the minimum volume for the cleanup.
    It is important to still have a minimum volume to deal with any unusual
    behaviour at small volumes, with the fluctuations being much larger
    relative to the overall size.

--- src/melting_rates.py
+++ src/melting_rates.py
@@ -137,7 +137,7 @@ def melting(infile, outfile, skip_frames):
 def clean(infile):
     infile = Path(infile)
     df = pd.read_hdf(infile, "fractions")
-    df = df.query("volume > 2000").query("time > 0")
+    df = df.query("volume > 100").query("time > 0")
     df["radius"] = np.sqrt(df["volume"].values) / np.pi
     df.to_hdf(
         infile.with_name(infile.stem + "_clean" + ".h5"), "fractions", format="table"
