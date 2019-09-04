9262475adac28f3ec9e4bf93c1662eae1dd1a16d
Malcolm Ramsay <malramsay64@gmail.com>
2019-09-05 09:55:28 +1000

    analysis(fluctuations): Aggregation as part of collation
    This moves the act of collating the data for each trajectory into the
    step of collating the data. This is to help solve some of the issues I
    have been having with running out of memory while processing the
    dataset.

--- src/fluctuations.py
+++ src/fluctuations.py
@@ -16,11 +16,12 @@ which are more flexible will be able to rapidly change configuration.
 """
 
 import logging
+from typing import Tuple
 
 import click
-import gsd
 import numpy as np
 import pandas as pd
+from pandas.api.types import CategoricalDtype
 from sdanalysis import order
 from sdanalysis.read import open_trajectory
 from sdanalysis.util import get_filename_vars
@@ -34,20 +35,54 @@ def main():
     pass
 
 
+bins = np.linspace(0, 1, 10001)
+bin_values = (bins[1:] + bins[:-1]) / 2
+
+
+def aggregate(values: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    """Convert values to a histogram of bins and values
+
+    This takes a collection of values in the range [0,1], binning values into a
+    histogram. Values are binned in increments of 1a-4, with only the bins containing values being returned.
+
+    Args:
+        values: A collection of values which will be binned into 
+
+    Returns:
+        centers: The centers of each of the bins
+        counts: The count of each bin
+
+    """
+    hist = np.histogram(values, bins=bins, density=True)[0]
+    non_zero = np.nonzero(hist)
+    return bin_values[non_zero], hist[non_zero]
+
+
 @main.command()
 @click.argument("output", type=click.Path(file_okay=True, dir_okay=False))
 @click.argument(
     "infiles", nargs=-1, type=click.Path(exists=True, file_okay=True, dir_okay=False)
 )
-@click.argument()
 def collate(output, infiles):
     with pd.HDFStore(output, "w") as dst:
         for file in infiles:
+            print(file)
             with pd.HDFStore(file) as src:
                 key = "ordering"
                 df = src.get(key)
+                bin_values, count = aggregate(df["orientational_order"])
+
+                df = pd.DataFrame(
+                    {
+                        "temperature": float(df["temperature"].values[0]),
+                        "pressure": float(df["pressure"].values[0]),
+                        "crystal": df["crystal"].values[0],
+                        "bins": bin_values,
+                        "count": count,
+                    }
+                )
                 df["crystal"] = df["crystal"].astype(
-                    CategoricalDtype(categories=["p2", "pg", "p2gg", "liquid"])
+                    CategoricalDtype(categories=["p2", "p2gg", "pg", "liquid"])
                 )
                 dst.append(key, df)
 
